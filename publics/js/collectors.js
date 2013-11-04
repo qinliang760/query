bfd767fa1898e05387008f71ea24e10f44acbb07
qinliang <qinliang760@163.com>
2013-11-04 09:12:45 +0800

    add query 2013.11.04

---
+++ publics/js/collectors.js
@@ -0,0 +1,312 @@
+
+var Collectors = {
+  init: function() {
+    this.getCollectors();
+    this.addCollectors();
+    this.delCollectors();
+    this.editCollectors();
+    Common.delCookie();
+    Common.login();
+    this.getHistory();
+  },
+  addCollectors:function(){
+    var add=$("#addCollectors");
+
+    add.click(function(){
+      var html='<input class="colName popInput" placeholder="请输入收集器名称" type="text" value=""/><input class="colUrl popInput" placeholder="请输入收集器地址" type="text" value=""/><p><a id="colSaveBtn" class="btn_blue_long" href="javascript:void(0);">确定</a></p>';
+      Common.setPop("<span class='sqlIcon tipIcon'></span>添加数据收集器",html);
+      Common.setInput();
+    })
+
+    $("#colSaveBtn").live("click",function(){
+        var name=$(".colName").val(),
+            url=$(".colUrl").val();
+        $.ajax({
+              url: "/sql/collectors/",
+              type: 'post',
+              data:{
+                        "name": name,
+                        "url": url,
+                        "timestamp": Common.getTimes()
+              },
+              dataType: 'json',
+              error: function(){},
+              success: function(data){
+
+              },
+              statusCode:{
+                201:function(){Boxy.alert("添加成功",function(){Query.delBoxy();Collectors.getCollectors();})},
+                401:function(){},
+                409:function(){}
+              }
+
+          });
+    })
+
+
+  },
+  getCollectors:function(){
+        $.ajax({
+              url: "/sql/collectors/",
+              type: 'get',
+              data:{
+                        "timestamp": Common.getTimes()
+              },
+              dataType: 'json',
+              error: function(){},
+              success: function(data){
+
+              },
+              statusCode:{
+                401:function(){alert("暂时没有结果")},
+                200:function(data){
+                  if(!data.length){return;}
+                  var titles=["name", "url", "status", "recent-sync", "操作"];
+                  var v=[],stop=[];
+                  for (var j=0,l=data.length;j<l;j++){
+                      v[j] = [];
+                      v[j].push(data[j].name);
+                      v[j].push(data[j].url);
+
+                      if (data[j].status == "no-sync") {
+                        v[j].push("待同步");
+                      } else if (data[j].status == "running") {
+                        v[j].push("运行中");
+                      } else if (data[j].status == "stopped") {
+                        v[j].push("已停止");
+                        stop.push(data[j].name)
+                      } else if (data[j].status == "abandoned") {
+                        v[j].push("已废弃");
+                      }
+
+                      if (data[j]["recent-sync"] == undefined || data[j]["recent-sync"] == null) {
+                        v[j].push('--');
+                      } else {
+                        v[j].push(new Date(data[j]["recent-sync"]).toLocaleString())
+                      }
+
+                      var del=Collectors.getTablesOp("del",data[j].id);
+                      var edit=""
+                      if(data[j].status=="no-sync"){
+                        var edit=Collectors.getTablesOp("edit",data[j].id+"__"+data[j].name+"__"+data[j].url)
+                      }
+                      v[j].push(del+" "+edit);
+                  }
+
+
+                  if(stop.length){
+                    var tipHtml='<span class="sqlIcon tipsIcon"></span>'+
+                    '您的'+stop.slice(0,3).join(",")+'已停止工作，部分数据没有同步，请通知您的系统管理员帮助重新开启数据收集器。'+
+                    '<a href="#">我知道了</a>';
+                    $(".colTips").html(tipHtml);
+                    $(".colTips a").live("click",function(){
+                      $(this).parent().html("");
+                    })                                        
+                  }                  
+                  
+                  titles.push();
+                  Common.setGrid(titles,v,"<span class='sqlIcon tipIcon'></span>常用查询");
+                }
+              }
+
+          });
+
+  },
+  delCollectors:function(){
+
+    var del=$(".colTable .collectorDel");
+
+    del.live("click",function(){
+        var id=$(this).attr("rel");
+
+      var html='<textarea class="colDelTa popTextarea" placeholder="删除理由" value=""/><p><a id="colDel" class="btn_blue_long" href="javascript:void(0);" rel="'+id+'">提交</a></p>';
+      Query.setPop("<span class='sqlIcon tipIcon tipCommonIcon'></span>删除收集器",html);
+      
+    })
+
+    $("#colDel").live("click",function(){
+        var id=$(this).attr("rel"),textareaVal=$(".colDelTa").val();
+        if(textareaVal==""){
+          Boxy.alert("请输入理由");
+          return false;
+        }
+        $.ajax({
+              url: "/sql/collectors/"+id,
+              type: 'delete',
+              data:{
+                        "timestamp": Common.getTimes()
+              },
+              dataType: 'json',
+              error: function(){},
+              success: function(data){
+
+              },
+              statusCode:{
+                404:function(){},
+                401:function(){},
+                200:function(){
+                  Query.delBoxy();
+                  Collectors.getCollectors();
+                }
+              }
+
+          });
+    })
+
+  },
+  editCollectors:function(){
+    var edit=$(".colTable .collectorEdit");
+
+    edit.live("click",function(){
+        var rel=$(this).attr("rel"),
+            data=rel.split("__");
+
+        var id=data[0],name=data[1],url=data[2];
+
+      var html='<input type="hidden" id="colEditId" value="'+id+'"><input class="colName popInput" type="text" value="'+name+'"/><input class="colUrl popInput" type="text" value="'+url+'"/><p><a id="colEditBtn" class="btn_blue_long" href="javascript:void(0);">确定</a></p>';
+      Common.setPop("<span class='sqlIcon tipIcon'></span>修改路径",html);
+    });
+
+     $("#colEditBtn").live("click",function(){
+        $.ajax({
+              url: "/sql/collectors/"+ $("#colEditId").val(),
+              type: 'put',
+              data:{
+                        "name": $(".colName").val(),
+                        "url": $(".colUrl").val()
+              },
+              dataType: 'json',
+              error: function(){},
+              success: function(data){
+
+              },
+              statusCode:{
+                404:function(){
+                  Boxy.alert("该收集器不存在");
+                },
+                401:function(){},
+                403:function(){},
+                409:function(){
+                  Boxy.alert("重复的收集器名称或url");
+                },
+                200:function(data){
+                  Query.delBoxy();
+                  Collectors.getCollectors();
+                },
+                500:function(){
+                  Boxy.alert("服务器错误");
+                }
+              }
+
+          });
+
+     });
+
+  },
+  getTablesOp:function(type,value){
+    var opName="",cls="";
+    if(type=="del"){
+      opName="废弃";
+      cls="collectorDel";
+    }else if(type=="edit"){
+      opName="修改";
+      cls="collectorEdit";
+    }
+    var html='<a class='+cls+' rel="'+value+'" href="javascript:void(0);">'+opName+'</a>';
+    return html;
+  },
+  getHistory:function(){
+    var obj=$("#checkCollectors");
+    obj.click(function(){
+        $.ajax({
+              url: "/sql/collectors/",
+              type: 'get',
+              data:{
+                        "timestamp": Common.getTimes()
+              },
+              dataType: 'json',
+              error: function(){},
+              success: function(data){
+
+              },
+              statusCode:{
+                401:function(){alert("暂时没有结果")},
+                200:function(data){
+                  if(!data.length){return;}
+                  var titles=[],v=[],data_id=[];
+
+                  for (var j=0,l=data.length;j<l;j++){
+                      if(data[j].status=="abandoned"){
+                            titles[j]=[];
+                            v[j]=[];                        
+
+                            for (var i in data[j]){
+                                if(i=="id"){
+                                  data_id.push(data[j][i]);
+                                  delete data[j][i];
+                                }else{
+                                  titles[j].push(i);
+                                  v[j].push(data[j][i]);                    
+                                }                                         
+
+                            }
+
+                      }                   
+              
+                  }
+
+                  Collectors.setPopGrid(titles,v,"<span class='sqlIcon tipIcon'></span>收集器历史操作");
+                }
+              }
+
+          });
+    })
+  },
+  setPopGrid:function(title,value,poptitle,option){//列名，值，弹窗标题，是否要开启选项
+  var params={
+    "reason":"备注",
+    "name":"数据收集器名称",
+    "recent-sync":"删除时间",
+    "url":"收集器地址",
+    "status":"状态"
+  }    
+      var values=Common.formatArr(value),column=Common.formatArr(title);
+
+      column=column[0];
+
+      var allColumn=[];
+
+      for (var i=0,l=column.length;i<l;i++){
+        allColumn.push({"sTitle":params[column[i]]});
+      }
+
+      var popHtml='<div id="pop"><table cellpadding="0" cellspacing="0" border="0" id="popGrid"></table></div>';
+
+      //var popHtml=$("#pop");
+      new Boxy(popHtml, {title: poptitle,closeText:"",modal:true,unloadOnHide:true});
+
+
+
+        $('#popGrid').dataTable({
+          "bProcessing": true,
+          "sPaginationType": "full_numbers",
+          bLengthChange:false,
+          bSort:false,
+          "aaData": values,
+          "aoColumns":allColumn,
+          sScrollY:"300px",
+          oLanguage:{
+            "sInfo": "共 _TOTAL_ 条记录 _START_ 到 _END_ ",
+            "sSearch":"搜索：",
+            "sProcessing":"加载中...",
+            "sZeroRecords":"无记录",
+            "oPaginate": {
+              "sFirst": "首页",
+              "sLast": "尾页",
+              "sNext": "下一页",
+              "sPrevious": "上一页"
+            }                    
+          }     
+        });
+    }  
+}
---
+++ publics/js/common.js
@@ -0,0 +1,246 @@
+var Common={
+	init:function(){
+		this.setInput();		
+	},
+	uiInit:function(){
+		this.selectDown();
+	},
+	setInput:function(){
+		$.sc2.placeholder({trigger:".popInput"});
+
+	},
+	  login:function(){
+	    var login_btn=$(".loginBtn");
+	    login_btn.live("click",function(){
+	      var html='<div class="loginPop">'+
+	      '<input type="email" name="email" value="" placeholder="请输入您的用户名" class="boxy-content popInput">'+
+	      '<input type="password" name="password" value="" placeholder="请输入您的密码" class="boxy-content popInput">'+
+	      '<a class="loginPost btn_blue_long" href="#">登录</a>'+
+	      '</div>';
+
+	      Common.setPop("<span class='sqlIcon tipIcon tipPerIcon'></span>登录 WhaleMiner",html);
+	      Common.setInput();
+	      return false;
+	    })
+
+	    $(".loginPost").live("click",function(){
+	        var email=$(".loginPop > input:eq(0)").val(),
+	            password=$(".loginPop > input:eq(1)").val();
+	        $.ajax({
+	            url: '/sql/',
+	            type: 'post',
+	            data:{
+	                      "email":email,
+	                      "password":password
+	            },
+	            dataType: 'json',
+	            error: function(){},
+	            success: function(data){
+
+	            },
+	            statusCode:{
+	              201:function(){location.href=location.href;},
+	              401:function(){Boxy.alert("用户名或密码不正确")}
+	            }
+
+	        });
+	    })
+	  },
+	delCookie:function(){
+		var delLink=$(".loginOut");
+		    var uid=$.cookie("user_id");
+		    if(uid){
+		      delLink.find("strong").html('退出登录');
+		    }else{
+		    	location.href="/sql/home.html";
+		    	//delLink.find("strong").html('<a href="#" class="loginBtn">登录</a>');
+		    }	
+		
+		delLink.click(function(){
+			//$(this).find("strong").html('<a href="#" class="loginBtn">登录</a>');
+			$.cookie("user_id",null,{path:"/sql/"});
+			location.href="/sql/home.html";
+		})
+	},
+	getTimes:function(){
+		return new Date().getTime();
+	},
+	setTooltips:function(obj,html){
+	    obj.tooltips({
+	      html: true,
+	      placement: "bottom",
+	      title: html,
+	      delay: {
+	        show: 100,
+	        hide: 1000
+	      }
+	    });
+	    		
+	},
+	getSelectTime:function(from,mainName){
+		var startTime=from;
+		
+		Common.timer[mainName]=setInterval(function(){
+			var currentTime=Common.getTimes(),
+				ms=currentTime-startTime;
+
+			var hLeave=ms%(24*3600*1000),
+				remainH=Math.floor(hLeave/(3600*1000));
+
+			var mLeave=hLeave%(3600*1000),
+				remainM=Math.floor(mLeave/(60*1000));
+
+			var sLeave=mLeave%(60*1000),
+				remainS=Math.round(sLeave/1000);
+
+				remainH<=9?remainH="0"+remainH:remainH=remainH;
+				remainM<=9?remainM="0"+remainM:remainM=remainM;
+				remainS<=9?remainS="0"+remainS:remainS=remainS;
+
+				$("#sqlTab .body").find(".main[name="+mainName+"] .sqlTime").html(remainH+":"+remainM+":"+remainS);							
+		},1000);
+
+	},
+	formatTime:function(time){
+
+		var date=new Date(time),
+			y=date.getFullYear(),
+			m=date.getMonth(),
+			d=date.getDate(),
+			h=date.getHours(),
+			mi=date.getMinutes(),
+			s=date.getSeconds();
+
+				m<12?m=m+1:m=1;
+				h<=9?h="0"+h:h=h;
+				mi<=9?mi="0"+mi:mi=mi;
+				s<=9?s="0"+s:s=s;			
+
+			return y+"-"+m+"-"+d+" "+h+":"+mi+":"+s;
+	},
+	isLogin:function(){
+        $.get(
+        "/sql/", {
+          "timestamp": Common.getTimes()
+        },
+        function(data,status) {
+
+        },
+        "json");
+	},
+	slideClick:function(){
+		var t=$(".sqlLogLink a");
+		t.live("click",function(){
+			var text=$(this).text();
+			if(text=="收起日志"){
+				$(this).find("strong").text("展开日志");
+			}else{
+				$(this).find("strong").text("收起日志");
+			}
+			$(this).toggleClass("active");
+			$(this).parent().next().stop().slideToggle();
+			return false;
+		})
+	},
+	toBai:function(num,total){
+		return Math.round(num / total * 10000) / 100.00 + "%";
+	},
+  setPop:function(poptitle,html){
+    new Boxy(html, {title: poptitle,closeText:"",modal:true,unloadOnHide:true});
+  },
+  setGrid:function(title,value,poptitle){//列名，值，弹窗标题，是否要开启选项
+
+	var params={
+	  "id":"查询ID",
+	  "name":"数据收集器名称",
+	  "recent-sync":"最后一次同步时间",
+	  "synced-data":"数据传输总量",
+	  "url":"收集器路径",
+	  "status":"状态",
+	  "reason":"原因",
+	  "操作":"操作"
+	}    
+    var values=value,
+        column=title;
+    var allColumn=[];
+    for (var i=0,l=column.length;i<l;i++){
+      allColumn.push({"sTitle":params[column[i]]});
+    }
+
+    var html='<table cellpadding="0" cellspacing="0" border="0" id="colGrid"></table>';
+    $("#sqlTableWrap").html(html);
+
+
+      $('#colGrid').dataTable({
+        "bProcessing": true,
+        "sPaginationType": "full_numbers",
+        bLengthChange:false,
+        bSort:true,
+        "aaSorting": [[ 2, "desc" ]],
+        bSortClasses:false,
+        "aaData": values,
+        "aoColumns":allColumn,
+        sScrollX:"100%",
+        oLanguage:{
+          "sInfo": "共 _TOTAL_ 条记录 _START_ 到 _END_ ",
+          "sSearch":"搜索：",
+          "sProcessing":"加载中...",
+          "oPaginate": {
+            "sFirst": "首页",
+            "sLast": "尾页",
+            "sNext": "下一页",
+            "sPrevious": "上一页"
+          }                    
+        }     
+      });
+
+
+
+  },
+  formatArr:function(arr){
+ 	  var column=[];	
+      for (var j=0,l=arr.length;j<l;j++){
+        if(typeof arr[j]!="undefined"){
+          column.push(arr[j]);
+        }
+      }
+      return column;
+  },
+  helpTabs:function(){
+	var helpList=$(".helpList a"),helpCon=$(".helpCon .helpDetail");
+
+	helpList.click(function(){
+		var index=helpList.index($(this));
+		$(this).parent().siblings().find("span").removeClass("openIcon").addClass("closeIcon");
+		$(this).find("span").removeClass("closeIcon").addClass("openIcon");
+		$(this).parent().siblings().removeClass("active");
+		$(this).parent().addClass("active");	
+		helpCon.eq(index).show().siblings().hide();
+
+		return false;
+
+	})  	
+  },
+  selectDown:function(){
+  	var selectDown=$(".topMenu1"),select=$(".selectMenu"),timer;
+  	selectDown.mouseenter(function(){
+  		$(this).addClass("activeDown");
+  		$(this).next().show();
+  	})
+  	selectDown.mouseleave(function(){
+  		var t=$(this);
+  		timer=setTimeout(function(){
+  			t.removeClass("activeDown");
+  			t.next().hide();
+  		},800)
+  	})  	
+
+  	select.hover(function(){
+  		clearTimeout(timer);
+  	},function(){
+  		$(this).hide();
+  		$(this).prev().removeClass("activeDown");
+  	})
+
+  }    	
+}
